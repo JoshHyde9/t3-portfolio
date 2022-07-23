41a9b5480cc7aef75e9a4a17bdf50d50821b8d36
Josh Hyde <joshhyde546@gmail.com>
2022-07-23 20:29:06 +1000

    Styling index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1 className="text-5xl">Hello from Index page</h1>;
+  return (
+    <div className="fade flex justify-center items-center flex-col h-full pb-20">
+      <h1 className="text-6xl mb-2">
+        👋, I'm <span className="text-purple-300">Josh</span>.
+      </h1>
+      <h2 className="text-4xl w-1/3 text-center">
+        Full Stack developer and I like to build things.
+      </h2>
+    </div>
+  );
 };
 
 export default Home;
