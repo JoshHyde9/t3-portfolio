6d85114fab621efb78ad5262d9fad72bc7335c1b
Josh Hyde <joshhyde546@gmail.com>
2022-07-23 13:16:07 +1000

    Styled form

--- src/pages/guestbook.tsx
+++ src/pages/guestbook.tsx
@@ -10,7 +10,7 @@ interface GuestBook {
 
 const Guest = (guest: GuestBook) => {
   return (
-    <div key={guest.id} className="px-4">
+    <div className="px-4">
       <h1 className="text-4xl">{guest.username}</h1>
       <p>{guest.comment}</p>
     </div>
@@ -25,15 +25,13 @@ const GuestBook: React.FC = () => {
     },
   });
 
-  console.log(data);
-
   const initialValues: GuestBook = {
     username: "",
     comment: "",
   };
 
   return (
-    <div className="container mx-auto">
+    <>
       <Formik
         initialValues={initialValues}
         onSubmit={async (values, { resetForm }) => {
@@ -41,14 +39,48 @@ const GuestBook: React.FC = () => {
           resetForm();
         }}
       >
-        <Form>
-          <Field name="username" placeholder="Username..." />
-          <Field
-            name="comment"
-            component="textarea"
-            placeholder="This is a really cool site..."
-          />
-          <button type="submit">Submit</button>
+        <Form className="w-full mx-auto max-w-lg">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="username"
+              >
+                Name:
+              </label>
+              <Field
+                name="username"
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-purple-500 transition ease-in-out duration-300"
+                placeholder="Bill Gates"
+                autoComplete="off"
+              />
+            </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="comment"
+              >
+                Comment:
+              </label>
+              <Field
+                name="comment"
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-purple-500 transition ease-in-out duration-300"
+                as="textarea"
+                rows="5"
+                placeholder="This is a really cool website"
+              />
+            </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-6 px-3">
+            <button
+              className="w-full shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded transition ease-in-out duration-300"
+              type="submit"
+            >
+              Comment
+            </button>
+          </div>
         </Form>
       </Formik>
       {error
@@ -57,17 +89,17 @@ const GuestBook: React.FC = () => {
           : ""
         : ""}
 
-      <div className="flex flex-wrap flex-row justify-center">
+      <div className="flex flex-wrap flex-col justify-center md:flex-row">
         {isLoading ? (
           <h1>Loading...</h1>
         ) : (
           data &&
           data.map((guest) => {
-            return <Guest {...guest} />;
+            return <Guest key={guest.id} {...guest} />;
           })
         )}
       </div>
-    </div>
+    </>
   );
 };
 
