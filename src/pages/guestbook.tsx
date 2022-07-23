1270991150446b63d6edb4363392c192bc14e3ea
Josh Hyde <joshhyde546@gmail.com>
2022-07-23 12:33:54 +1000

    Guest book data is now displayed

--- src/pages/guestbook.tsx
+++ src/pages/guestbook.tsx
@@ -1,17 +1,29 @@
-import type { NextPage } from "next";
+import React from "react";
 import { trpc } from "../utils/trpc";
 import { Field, Form, Formik } from "formik";
 
-interface createGuestBook {
+interface GuestBook {
+  id?: string;
   username: string;
   comment: string;
 }
 
-const GuestBook: NextPage = () => {
-  const { mutate, error } = trpc.useMutation(["guestbook.create"]);
-  const guests = trpc.useQuery(["guestbook.getAll"], {});
+const Guest = (guest: GuestBook) => {
+  return (
+    <div key={guest.id}>
+      <h1 className="text-4xl">{guest.username}</h1>
+      <p>{guest.comment}</p>
+    </div>
+  );
+};
 
-  const initialValues: createGuestBook = {
+const GuestBook: React.FC = () => {
+  const { mutate, error } = trpc.useMutation(["guestbook.create"]);
+  const { data, isLoading } = trpc.useQuery(["guestbook.getAll"]);
+
+  console.log(data);
+
+  const initialValues: GuestBook = {
     username: "",
     comment: "",
   };
@@ -39,7 +51,17 @@ const GuestBook: NextPage = () => {
           ? "Please fill in all fields."
           : ""
         : ""}
-      .
+
+      <div className="flex flex-wrap flex-row justify-center">
+        {isLoading ? (
+          <h1>Loading...</h1>
+        ) : (
+          data &&
+          data.map((guest) => {
+            return Guest(guest);
+          })
+        )}
+      </div>
     </div>
   );
 };
