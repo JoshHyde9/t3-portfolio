e0f0404119876c1054f1e55cb24b9c6f10409ba5
Josh Hyde <joshhyde546@gmail.com>
2022-07-23 11:47:05 +1000

    Created backend

--- src/server/router/guestbook.ts
+++ src/server/router/guestbook.ts
@@ -2,15 +2,27 @@ import { createRouter } from "./context";
 import { z } from "zod";
 
 const createGuestBookSchema = z.object({
-  username: z.string(),
-  commemt: z.string(),
+  username: z.string().min(1, { message: "Please fill in required field." }),
+  comment: z.string().min(1, { message: "Please fill in required field." }),
 });
 
-export const guestBook = createRouter().mutation("create", {
-  input: createGuestBookSchema,
-  async resolve({ input }) {
-    const { username, commemt } = input;
+export const guestBook = createRouter()
+  .mutation("create", {
+    input: createGuestBookSchema,
+    async resolve({ input, ctx }) {
+      const { username, comment } = input;
 
-    return console.log("Inputted: ", { username, commemt });
-  },
-});
+      const newGuest = await ctx.prisma.guestBook.create({
+        data: { username, comment },
+      });
+
+      return newGuest;
+    },
+  })
+  .query("getAll", {
+    async resolve({ ctx }) {
+      const allGuests = await ctx.prisma.guestBook.findMany();
+
+      return allGuests;
+    },
+  });
