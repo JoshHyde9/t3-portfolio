58b6f8550bdf17deaee4707b9682b7d8941b61da
Josh Hyde <joshhyde546@gmail.com>
2022-07-23 14:41:26 +1000

    Added loading spinner

--- src/pages/guestbook.tsx
+++ src/pages/guestbook.tsx
@@ -34,7 +34,7 @@ const GuestBook: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto max-w-lg">
+    <div className="container mx-auto max-w-lg px-2">
       <h1 className="text-2xl text-purple-300 mt-2">Guestbook</h1>
       <p className="leading-tight">
         Please feel free to leave a comment now that you have found this page!
@@ -46,7 +46,7 @@ const GuestBook: React.FC = () => {
           resetForm();
         }}
       >
-        <Form className="w-full mt-10">
+        <Form className="mx-auto w-full mt-10">
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full px-3">
               <label
@@ -105,9 +105,29 @@ const GuestBook: React.FC = () => {
         </Form>
       </Formik>
 
-      <div className="flex flex-wrap flex-col justify-center md:flex-row">
+      <div className="w-full flex flex-col flex-wrap justify-center">
         {isLoading ? (
-          <h1>Loading...</h1>
+          <div className="flex justify-center">
+            <svg
+              className="animate-spin -ml-1 mr-3 h-7 w-7 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75 fill-purple-500"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          </div>
         ) : (
           data &&
           data.map((guest) => {
