96b7e73df5d4dee6d8e02b65426860841d344bf5
Josh Hyde <joshhyde546@gmail.com>
2022-07-24 16:14:44 +1000

    Now displays creation date

--- src/pages/guestbook.tsx
+++ src/pages/guestbook.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { trpc } from "../utils/trpc";
 import { Field, Form, Formik } from "formik";
+import dayjs from "dayjs";
 
 interface GuestBook {
   id?: string;
   username: string;
   comment: string;
+  createdAt?: Date;
 }
 
 const Guest = (guest: GuestBook) => {
   return (
-    <div className="pb-4 max-w-md">
-      <p className="text-lg">{guest.comment}</p>
-      <h1 className="text-xl text-purple-300">
-        <span className="italic">&#8211; </span>
-        {guest.username}
-      </h1>
+    <div className="pb-4 w-full">
+      <div className="flex flex-row justify-between">
+        <div className="w-2/3">
+          <p className="text-lg">{guest.comment}</p>
+          <h1 className="text-xl text-purple-300">
+            <span className="italic">&#8211; </span>
+            {guest.username}
+          </h1>
+        </div>
+        <p className="text-sm text-neutral-400">
+          {dayjs(guest.createdAt).format("DD/MM/YYYY - HH:mm")}
+        </p>
+      </div>
     </div>
   );
 };
