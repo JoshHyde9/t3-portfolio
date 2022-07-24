02d5eeef79cd4dd3df9186a5dade5e9f38499540
Josh Hyde <joshhyde546@gmail.com>
2022-07-24 10:32:55 +1000

    Setuo mailing route

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -3,10 +3,12 @@ import { createRouter } from "./context";
 import superjson from "superjson";
 
 import { guestBook } from "./guestbook";
+import { email } from "./email";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("guestbook.", guestBook);
+  .merge("guestbook.", guestBook)
+  .merge("email.", email);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
