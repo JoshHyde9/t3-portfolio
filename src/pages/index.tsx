f0d6e5d66646d01d0b047a8aa54ac2f9e32e2233
Josh Hyde <joshhyde546@gmail.com>
2022-07-24 21:20:31 +1000

    Added more SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,10 +25,28 @@ const Home: NextPage = () => {
           name="og:description"
           content="A handy dandy website for displaying my accomplishments, skills, experiences, and attributes"
         />
+        <meta name="og:site_name" content="Josh Hyde | Full Stack Developer" />
         <meta name="author" content="Josh Hyde" />
         <meta name="og:author" content="Josh Hyde" />
-        <meta property="image" content="/apple-touch-icon.png" />
-        <meta property="og:image" content="/apple-touch-icon.png" />
+        <meta property="url" content="https://joshhyde.me/" />
+        <meta property="og:url" content="https://joshhyde.me/" />
+        <meta property="type" content="website" />
+        <meta property="og:type" content="website" />
+        <meta
+          name="twitter:card"
+          content="https://avatars.githubusercontent.com/u/40751087?v=4"
+        />
+        <meta name="twitter:site" content="@JoshHyde9" />
+        <meta
+          property="image"
+          content="https://avatars.githubusercontent.com/u/40751087?v=4"
+        />
+        <meta
+          property="og:image"
+          content="https://avatars.githubusercontent.com/u/40751087?v=4"
+        />
+        <meta property="image:alt" content="Josh Hyde" />
+        <meta property="og:image:alt" content="Josh Hyde" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
