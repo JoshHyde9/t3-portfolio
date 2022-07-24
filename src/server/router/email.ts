910c1a892e8f1c79d3439f96eac5263c2592d0cb
Josh Hyde <joshhyde546@gmail.com>
2022-07-24 10:33:32 +1000

    Created send email mutation

---
+++ src/server/router/email.ts
@@ -0,0 +1,17 @@
+import { createRouter } from "./context";
+import { z } from "zod";
+import { sendEmail } from "../../utils/sendEmail";
+
+const createEmailSchema = z.object({
+  subject: z.string().min(1, { message: "Please fill in required field." }),
+  message: z.string().min(1, { message: "Please fill in required field." }),
+});
+
+export const email = createRouter().mutation("sendEmail", {
+  input: createEmailSchema,
+  async resolve({ input }) {
+    const { subject, message } = input;
+
+    await sendEmail(subject, message);
+  },
+});
