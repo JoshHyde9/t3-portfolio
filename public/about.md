e36bc9cefe9e2a91200c124fe14bf5670e7a791d
Josh Hyde <joshhyde546@gmail.com>
2022-07-24 14:04:15 +1000

    Started about me section

---
+++ public/about.md
@@ -0,0 +1,20 @@
+# About me
+
+For those who may not know, my name is Josh, but people on the internet call me Jim. I am 20 years old and was born in Echuca, Victoria but now residing on the Mornington Peninsula. I am a passionate developer and have a big emphasis on producing quality products.
+
+&nbsp;
+&nbsp;
+
+I am a year 12 graduate and have enrolled myself into HarvardX's "Computer Science for Web Programming" and have completed the Computer Science section of the course and learnt a lot. I am glad to be able to use what I have learned and put that knowledge into my projects. If you're interested in working together or want to ask a question, please do not hesitate to [contact me](https://joshhyde.me/contact).
+
+&nbsp;
+&nbsp;
+
+## My Skills
+
+|            |             |         |            |
+| ---------- | ----------- | ------- | ---------- |
+| TypeScript | JavaScript  | React   | Nextjs     |
+| Redis      | GraphQL     | Nodejs  | PostgreSQL |
+| Nodejs     | TypeORM     | MongoDB | Firebase   |
+| tRPC       | TailwindCSS | Python  | SCSS       |
---
+++ src/pages/about.tsx
@@ -0,0 +1,55 @@
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+const About: NextPage = () => {
+  const [content, setContent] = useState<string>("");
+
+  useEffect(() => {
+    fetch("about.md")
+      .then((res) => res.text())
+      .then((text) => setContent(text));
+  });
+
+  return (
+    <div className="container mx-auto max-w-2xl px-2">
+      <ReactMarkdown
+        components={{
+          h1: ({ node, ...props }) => (
+            <h1 className="text-4xl text-purple-300" {...props} />
+          ),
+          h2: ({ node, ...props }) => (
+            <h2 className="text-3xl text-purple-300" {...props} />
+          ),
+          hr: ({ node, ...props }) => (
+            <hr className="border-t-1 border-neutral-600 m-2" {...props} />
+          ),
+          a: ({ node, ...props }) => (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-purple-500 hover:text-purple-300 transition ease-in-out duration-300"
+              {...props}
+            />
+          ),
+
+          table: ({ node, ...props }) => (
+            <table
+              className="w-full table-auto text-center md:text-left"
+              {...props}
+            />
+          ),
+          td: ({ node, isHeader = false, ...props }) => (
+            //@ts-ignore
+            <td className="py-1" {...props} />
+          ),
+        }}
+        children={content}
+        remarkPlugins={[remarkGfm]}
+      />
+    </div>
+  );
+};
+
+export default About;
