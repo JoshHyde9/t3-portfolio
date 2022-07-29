37c91d682cb4f2c5e740f453b8c6c5fccbcaeb49
JoshHyde9 <joshhyde546@gmail.com>
2022-07-30 09:03:45 +1000

    Added name field to email form

--- src/server/router/email.ts
+++ src/server/router/email.ts
@@ -5,13 +5,14 @@ import { sendEmail } from "../../utils/sendEmail";
 const createEmailSchema = z.object({
   subject: z.string().min(1, { message: "Please fill in required field." }),
   message: z.string().min(1, { message: "Please fill in required field." }),
+  name: z.string().min(1, { message: "Please fill in required field." }),
 });
 
 export const email = createRouter().mutation("sendEmail", {
   input: createEmailSchema,
   async resolve({ input }) {
-    const { subject, message } = input;
+    const { subject, message, name } = input;
 
-    await sendEmail(subject, message);
+    await sendEmail(subject, message, name);
   },
 });
--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -3,10 +3,14 @@ import { env } from "../server/env.mjs";
 
 const client = new SparkPost(env.SPARKPOST_API_KEY);
 
-export const sendEmail = async (subject: string, message: string) => {
+export const sendEmail = async (
+  subject: string,
+  message: string,
+  name: string
+) => {
   await client.transmissions.send({
     content: {
-      from: "Josh@hello.joshhyde.me",
+      from: `${name} <josh@hello.joshhyde.me>`,
       subject,
       html: message,
     },
