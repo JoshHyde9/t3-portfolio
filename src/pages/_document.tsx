5489d2488b1a814769a36e4d00d353d59992ae05
JoshHyde9 <joshhyde546@gmail.com>
2022-08-01 14:33:01 +1000

    Added lang to html element

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const AppDocument = () => {
+  return (
+    <Html lang="en-GB">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default AppDocument;
