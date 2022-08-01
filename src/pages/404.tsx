dd97a6721f56aad5c78bf4bbccaf2d6273c0c5a4
JoshHyde9 <joshhyde546@gmail.com>
2022-08-01 14:17:37 +1000

    Added 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from "next";
+import NextLink from "next/link";
+
+import SEO from "../components/SEO";
+
+const PageNotFound: NextPage = () => {
+  return (
+    <>
+      <SEO
+        title="Page Not Found | Josh Hyde"
+        url="https://joshhyde.me/about"
+        description="Uh oh! Either I have done something wrong or you have tried to do something cheeky."
+      />
+
+      <div className="flex flex-col justify-center items-center h-96">
+        <h1 className="text-4xl text-center text-purple-300 mb-2">Uh oh!</h1>
+        <h2 className="text-3xl text-center text-purple-300 mb-6 w-1/2">
+          Either I have done something wrong or you have tried to do something
+          cheeky.
+        </h2>
+
+        <NextLink href="/">
+          <a className="text-3xl transition ease-in-out duration-300 relative text-purple-500 hover:text-purple-300">
+            Back Home
+          </a>
+        </NextLink>
+      </div>
+    </>
+  );
+};
+
+export default PageNotFound;
