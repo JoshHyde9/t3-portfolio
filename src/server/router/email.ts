6c585fc1c0934d7c36916c386db19333080ef279
JoshHyde9 <joshhyde546@gmail.com>
2022-08-31 13:19:49 +1000

    Added email to schema

--- src/server/router/email.ts
+++ src/server/router/email.ts
@@ -3,16 +3,19 @@ import { z } from "zod";
 import { sendEmail } from "../../utils/sendEmail";
 
 const createEmailSchema = z.object({
+  name: z.string().min(1, { message: "Please fill in required field." }),
+  contactEmail: z
+    .string()
+    .email({ message: "Please input a valid email address. " }),
   subject: z.string().min(1, { message: "Please fill in required field." }),
   message: z.string().min(1, { message: "Please fill in required field." }),
-  name: z.string().min(1, { message: "Please fill in required field." }),
 });
 
 export const email = createRouter().mutation("sendEmail", {
   input: createEmailSchema,
   async resolve({ input }) {
-    const { subject, message, name } = input;
+    const { name, contactEmail, subject, message } = input;
 
-    await sendEmail(subject, message, name);
+    await sendEmail(subject, contactEmail, message, name);
   },
 });
