ec59c9676b42d2496a4d7f01dc125811a9c0f347
JoshHyde9 <joshhyde546@gmail.com>
2022-08-31 13:20:38 +1000

    Added contact email to submitted contact message

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -4,15 +4,16 @@ import { env } from "../server/env.mjs";
 const client = new SparkPost(env.SPARKPOST_API_KEY);
 
 export const sendEmail = async (
+  name: string,
+  contactEmail: string,
   subject: string,
-  message: string,
-  name: string
+  message: string
 ) => {
   await client.transmissions.send({
     content: {
       from: `${name} <josh@hello.joshhyde.me>`,
       subject,
-      html: message,
+      html: `<html><body><p>Contact email: ${contactEmail}</p><p>${message}</p></body></html>`,
     },
     recipients: [{ address: env.MY_EMAIL_ADDRESS }],
   });
