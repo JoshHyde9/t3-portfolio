668ff3fdbe842dfc9f9e30a36613d1dbc60114d9
JoshHyde9 <joshhyde546@gmail.com>
2023-01-13 10:33:57 +1100

    fix: changed email it is sent from

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -11,7 +11,7 @@ export const sendEmail = async (
 ) => {
   await client.transmissions.send({
     content: {
-      from: `${name} <josh@hello.joshhyde.me>`,
+      from: `${name} <hello@joshhyde.me>`,
       subject,
       html: `<html><body><p>Contact email: ${contactEmail}</p><p>${message}</p></body></html>`,
     },
